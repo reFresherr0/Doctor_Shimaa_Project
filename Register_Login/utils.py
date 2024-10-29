5defee7a38e29141a77e6cd176d3947f1f69d984
Ahmed Hassan <93086169+reFresherr0@users.noreply.github.com>
2024-10-29 22:42:25 +0300

    Delete Register_Login directory

--- Register_Login/utils.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import timezone
-
-class AccessTokenGenerator(PasswordResetTokenGenerator):
-    
-    def _make_hash_value(self, user, timestamp): 
-        return f'{user.email}{timestamp}{user.is_active}'
